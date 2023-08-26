360c0ca9e59101eed46b477ec256538d5fea9cf7
rhunter-3 <russell.hunter3@gmail.com>
2023-08-26 17:11:43 +1000

    Removing redundant update

--- jammy.sh
+++ jammy.sh
@@ -4,5 +4,3 @@ echo "deb [signed-by=/usr/share/keyrings/r-project.gpg] https://cloud.r-project.
 sudo apt install --no-install-recommends r-base r-base-dev gdebi-core git
 wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2023.06.2-561-amd64.deb
 sudo gdebi Downloads/rstudio-2023.06.2-561-amd64.deb
-
-sudo apt-get update
