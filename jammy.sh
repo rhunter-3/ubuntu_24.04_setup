b09c53cfe62996dc7e014f20528d25ef2885e0da
rhunter-3 <russell.hunter3@gmail.com>
2023-08-26 16:27:09 +1000

    Init commit

---
+++ jammy.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo gpg --dearmor -o /usr/share/keyrings/r-project.gpg
+echo "deb [signed-by=/usr/share/keyrings/r-project.gpg] https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" | sudo tee -a /etc/apt/sources.list.d/r-project.list
+sudo apt install --no-install-recommends r-base r-base-dev gdebi-core git
+wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2023.06.2-561-amd64.deb
+sudo gdebi Downloads/rstudio-2023.06.2-561-amd64.deb
+
