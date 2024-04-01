d758c5b58221968e879b006601104e8cba63224a
rhunter-3 <russell.hunter3@gmail.com>
2024-04-01 19:38:02 +1100

    Create docker_desktop.sh

---
+++ docker_desktop.sh
@@ -0,0 +1,29 @@
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# List the available versions:
+apt-cache madison docker-ce | awk '{ print $3 }'
+
+# 5:24.0.0-1~ubuntu.22.04~jammy
+# 5:23.0.6-1~ubuntu.22.04~jammy
+# ...
+
+VERSION_STRING=5:24.0.0-1~ubuntu.22.04~jammy
+sudo apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Convenience script install
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh --dry-run
