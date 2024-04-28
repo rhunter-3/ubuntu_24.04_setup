99f3de5976b9fd917e29a313244ed90024727ef8
rhunter-3 <russell.hunter3@gmail.com>
2024-04-28 20:35:21 +1000

    Update build_r_from_source.sh

--- build_r_from_source.sh
+++ build_r_from_source.sh
@@ -1,16 +1,21 @@
 # BUILD R FROM SOURCE ON UBUNTU
-# R-4.3.2
+# R-4.4.0
 
 # Install Linux dependencies
 # Make sure src repos are uncommented in /etc/apt/sources.list
+# Ubuntu sources have moved to /etc/apt/sources.list.d/ubuntu.sources in 24.04
 sudo apt-get build-dep r-base
 
 # Download and extract source code
-wget https://cran.r-project.org/src/base/R-4/R-4.3.2.tar.gz
-tar -xzvf R-4.3.2.tar.gz
-cd R-4.3.2
+wget https://cran.r-project.org/src/base/R-4/R-4.4.0.tar.gz
+tar -xzvf R-4.4.0.tar.gz
+cd R-4.4.0
 
 # Build R from source
 ./configure --prefix=/opt/R/$(cat VERSION) --enable-R-shlib --with-blas --with-lapack
 make
 sudo make install
+
+# Clean up
+rm ~/R-4.4.0.tar.gz
+rm -rf R-4.4.0/
